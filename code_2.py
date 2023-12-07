918e6a7391d5e7959af34d57bf06c1f98db193c6
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-12-07 18:28:26 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+    return data
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
+
+
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
+
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+
+def generate_random_data():
+import random
+    return data
+    main()
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    for item in data:
+
+    data = generate_random_data()            System.out.println("Random Number: " + item);
+
+
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        }
+        for (int item : data) {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+    }
+        }
+            data[i] = (int) (Math.random() * 100) + 1;    const data = generateRandomData();
+    data.forEach(item => {
+main();
+        console.log("Random Number: " + item);
+function main() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+}
+
+
+    const data = [];
+    return data;
+function generateRandomData() {
+    }
+    for (let i = 0; i < 10; i++) {
