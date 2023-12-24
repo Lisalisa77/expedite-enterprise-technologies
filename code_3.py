0f9d3858bd2835c622bb86b9fdec590994af576e
Lisa <143336008+Lisalisa77@users.noreply.github.com>
2023-12-24 13:30:33 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,51 +1,38 @@
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-def generate_random_data():
-    main()
-if __name__ == "__main__":
-    for item in data:
-def main():
-
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
-def main():
-    return data
-    for item in data:
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    main()
-if __name__ == "__main__":
-import random
-
-    }
-        }
-
-    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
 }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
+    }
         int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
         }
-        for (int item : data) {
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
 
-            data[i] = (int) (Math.random() * 100) + 1;    data = generate_random_data()
-if __name__ == "__main__":
-    for item in data:
-        print(f"Random Number: {item}")
+        }
 
+        for (int item : data) {def main():
 
-def generate_random_data():
+    data = generate_random_data()
     main()
-import random
-def main():
-
     return data
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+    for item in data:
+
+
+def generate_random_data():        for (int item : data) {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+
+}
+        }
+
+        }
